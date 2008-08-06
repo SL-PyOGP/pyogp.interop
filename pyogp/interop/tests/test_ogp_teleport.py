e08d21686e2d67108be392dfe1b9d9e5e9da0608
locklainn.linden <devnull@localhost>
2008-08-06 18:21:33 +0000

    ogp teleport test bugs fixed

--- pyogp/interop/tests/test_ogp_teleport.py
+++ pyogp/interop/tests/test_ogp_teleport.py
@@ -16,6 +16,12 @@ from pyogp.lib.base.message.message_system import MessageSystem
 from pyogp.lib.base.message.circuitdata import Host
 from pyogp.lib.base.message.message_types import MsgType
 
+from zope.component import provideUtility
+from pyogp.lib.base.network.interfaces import IUDPClient
+from pyogp.lib.base.network.net import NetUDPClient
+
+provideUtility(NetUDPClient(), IUDPClient)
+
 class OGPTeleportTest(unittest.TestCase):
 
     login_uri = None
@@ -60,13 +66,12 @@ class OGPTeleportTest(unittest.TestCase):
         place = IPlaceAvatar(agentdomain)
         
         avatar = place(region)
-        
-        #####################
-        # Begin message handling block
-        
-        agent.id = '7492c341-57a8-4256-abc2-21f28809855b'
-        # enusbot13's is 7492c341-57a8-4256-abc2-21f28809855b
-        
+
+        #import pprint
+        #pprint.pprint(avatar.region.details)
+
+        agent_id = avatar.region.details['agent_id']
+        #begin UDP communication
         host = Host(avatar.region.details['sim_ip'],
                     avatar.region.details['sim_port'])
 
@@ -79,7 +84,7 @@ class OGPTeleportTest(unittest.TestCase):
                                 uuid.UUID(avatar.region.details['session_id']), \
                                 MsgType.MVT_LLUUID)
         self.messenger.add_data('ID', \
-                                uuid.UUID(agent.id), \
+                                uuid.UUID(agent_id), \
                                 MsgType.MVT_LLUUID)
         self.messenger.send_message(host)
 
@@ -87,7 +92,7 @@ class OGPTeleportTest(unittest.TestCase):
         self.messenger.new_message("CompleteAgentMovement")
         self.messenger.next_block("AgentData")
         self.messenger.add_data('AgentID', \
-                                uuid.UUID(agent.id), \
+                                uuid.UUID(agent_id), \
                                 MsgType.MVT_LLUUID)
         self.messenger.add_data('SessionID', \
                                 uuid.UUID(avatar.region.details['session_id']), \
@@ -96,8 +101,10 @@ class OGPTeleportTest(unittest.TestCase):
                                 MsgType.MVT_U32)
         self.messenger.send_message(host)
 
+
+
         # To do: this test is failing, though the ad recieved the request. the derez avatar call fails....
-        time.sleep(10)
+        #time.sleep(10)
       
         tp_region = Region(self.target_region_uri)
         place = IPlaceAvatar(agentdomain)
@@ -117,8 +124,6 @@ class OGPTeleportTest(unittest.TestCase):
         assert avatar.region.details['session_id'] != None or avatar.region.details['session_id'] != {}, "Rez_avatar/place returned no session_id"
         assert avatar.region.details['secure_session_id'] != None or avatar.region.details['secure_session_id'] != {}, "Rez_avatar/place returned no secure_session_id" 
         assert avatar.region.details['circuit_code'] != None or avatar.region.details['circuit_code'] != {}, "Rez_avatar/place returned no cicuit_code"
-
-
         
        
     def tearDown(self):
