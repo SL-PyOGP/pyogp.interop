85c604e14fbd95767efeca7632dbd0e202efc4b2
enus.linden <devnull@localhost>
2008-09-20 05:04:31 +0000

    more bettererer teleport test

--- pyogp/interop/tests/test_ogp_teleport.py
+++ pyogp/interop/tests/test_ogp_teleport.py
@@ -18,18 +18,12 @@ http://svn.secondlife.com/svn/linden/projects/2008/pyogp/LICENSE.txt
 $/LicenseInfo$
 """
 
+from threading import Thread
 import unittest, doctest
 import ConfigParser
 from pkg_resources import resource_stream
 import time
 import uuid
-import pprint
-try:
-    import msvcrt #WINDOWS ONLY!
-    canInput = True
-except:
-    print "Trying to import a windows library"
-    canInput = False
 
 from pyogp.lib.base.credentials import PlainPasswordCredential
 from pyogp.lib.base.agentdomain import AgentDomain
@@ -48,22 +42,7 @@ from zope.component import provideUtility
 from pyogp.lib.base.network.interfaces import IUDPClient
 from pyogp.lib.base.network.net import NetUDPClient
 
-globals()["controlFlags"] = 0x00000000
-globals()["agentFlagSent"] = 0x00000000
-#global controlFlags = 0x00000000
-#global agentFlagSent = controlFlags
-
-STOP_MOVING = 0x00004000
-
-NUDGE_FORWARD = 0x00080000
-NUDGE_BACKWARD = 0x00100000
-NUDGE_LEFT = 0x00200000
-NUDGE_RIGHT = 0x00400000
-
-MOVE_FORWARD = 0x00000001 | 0x00000400
-MOVE_BACKWARD = 0x00000002 | 0x00000400
-MOVE_LEFT = 0x00000004 | 0x00000800
-MOVE_RIGHT = 0x00000001 | 0x00000008
+import helpers
 
 class OGPTeleportTest(unittest.TestCase):
 
@@ -103,67 +82,161 @@ class OGPTeleportTest(unittest.TestCase):
 
         #gets seedcap, and an agent that can be placed in a region
         agent = agentdomain.login(credentials)
-        assert agentdomain.seed_cap != None or agentdomain.seed_cap != {}, "Login to agent domain failed"
 
+        #establish AD event queue connection
+        ad_event_queue = IEventQueueGet(agentdomain)
+        ad_queue_thread = Thread(target=run_ad_eq, name="Agent Domain event queue", args=(ad_event_queue,))
+        ad_queue_thread.start()
+        
         #gets the rez_avatar/place cap
         caps = agentdomain.seed_cap.get(['rez_avatar/place'])
         
-        assert caps['rez_avatar/place'] != None or caps['rez_avatar/place'] != {}, "Failed to retrieve the rez_avatar/place capability"
-       
-        # try and connect to a sim
+        # try and rez on a sim
         region = Region(self.start_region_uri)
         place = IPlaceAvatar(agentdomain)
-        
         avatar = place(region)
-
-        assert avatar.region.details['seed_capability'] != None or avatar.region.details['seed_capability'] != {}, "Rez_avatar/place returned no seed cap"
-        assert avatar.region.details['look_at'] != None or avatar.region.details['look_at'] != {}, "Rez_avatar/place returned no look_at"
-        assert avatar.region.details['sim_ip'] != None or avatar.region.details['sim_ip'] != {}, "Rez_avatar/place returned no sim_ip"
-        assert avatar.region.details['sim_port'] != None or avatar.region.details['sim_port'] != {}, "Rez_avatar/place returned no sim_port"
-        assert avatar.region.details['region_x'] != None or avatar.region.details['region_x'] != {}, "Rez_avatar/place returned no region_x"
-        assert avatar.region.details['region_y'] != None or avatar.region.details['region_y'] != {}, "Rez_avatar/place returned no region_y"
-        assert avatar.region.details['region_id'] != None or avatar.region.details['region_id'] != {}, "Rez_avatar/place returned no region_id"
-        assert avatar.region.details['sim_access'] != None or avatar.region.details['sim_access'] != {}, "Rez_avatar/place returned no sim_access"
-        assert avatar.region.details['connect'] != None or avatar.region.details['connect'] != {}, "Rez_avatar/place returned no connect"
-        assert avatar.region.details['position'] != None or avatar.region.details['position'] != {}, "Rez_avatar/place returned no position"
-        assert avatar.region.details['session_id'] != None or avatar.region.details['session_id'] != {}, "Rez_avatar/place returned no session_id"
-        assert avatar.region.details['secure_session_id'] != None or avatar.region.details['secure_session_id'] != {}, "Rez_avatar/place returned no secure_session_id" 
-        assert avatar.region.details['circuit_code'] != None or avatar.region.details['circuit_code'] != {}, "Rez_avatar/place returned no cicuit_code"
-
-        self.agent_id = avatar.region.details['agent_id']
-        self.session_id = avatar.region.details['session_id']
-        self.circuit_code = avatar.region.details['circuit_code']
         
+        # grab some data before it disappears
+        vanishing_data = {}
+        vanishing_data['agent_id'] = avatar.region.details['agent_id']
+        vanishing_data['session_id'] = avatar.region.details['session_id']
+        vanishing_data['circuit_code'] = avatar.region.details['circuit_code']        
+                
+        # grab some packets from sim1
+        packets_captured = self.maintain_sim_presence(vanishing_data, region, place, avatar, 5)
+
+        # try and rez on sim2
+        region2 = Region(self.target_region_uri)
+        place2 = IPlaceAvatar(agentdomain)
+        avatar2 = place(region2)
+
+        # grab some packets from sim2
+        packets_captured2 = self.maintain_sim_presence(vanishing_data, region2, place2, avatar2, 15)
+        
+        # now let's validate some shizz
+        assert avatar.region.details['seed_capability'] != avatar2.region.details['seed_capability']
+        assert packets_captured2['TeleportProgress'] == 1
+        assert packets_captured2['AgentMovementComplete'] == 1
+ 
+    def test_teleport_same_sim(self):
+    
+        credentials = PlainPasswordCredential(self.firstname, self.lastname, self.password)
+        agentdomain = AgentDomain(self.login_uri)
+
+        #gets seedcap, and an agent that can be placed in a region
+        agent = agentdomain.login(credentials)
+
+        #establish AD event queue connection
+        ad_event_queue = IEventQueueGet(agentdomain)
+        ad_queue_thread = Thread(target=run_ad_eq, name="Agent Domain event queue", args=(ad_event_queue,))
+        ad_queue_thread.start()
+        
+        #gets the rez_avatar/place cap
+        caps = agentdomain.seed_cap.get(['rez_avatar/place'])
+        
+        # try and rez on a sim
+        region = Region(self.start_region_uri)
+        place = IPlaceAvatar(agentdomain)
+        avatar = place(region, [5,5,5])
+        
+        # grab some data before it disappears
+        vanishing_data = {}
+        vanishing_data['agent_id'] = avatar.region.details['agent_id']
+        vanishing_data['session_id'] = avatar.region.details['session_id']
+        vanishing_data['circuit_code'] = avatar.region.details['circuit_code']        
+                
+        # grab some packets from sim1
+        packets_captured = self.maintain_sim_presence(vanishing_data, region, place, avatar, 5)
+
+        # try and rez on sim2
+        region2 = Region(self.target_region_uri)
+        place2 = IPlaceAvatar(agentdomain)
+        avatar2 = place(region2, [255,255,255])
+
+        # grab some packets from sim2
+        packets_captured2 = self.maintain_sim_presence(vanishing_data, region2, place2, avatar2, 5)
+        
+        # now let's validate some shizz
+        # assert avatar.region.details['seed_capability'] != avatar2.region.details['seed_capability']
+        assert packets_captured2['TeleportProgress'] == 1
+        assert packets_captured2['AgentMovementComplete'] == 1       
+
+    def test_teleport_same_sim_position(self):
+    
+        credentials = PlainPasswordCredential(self.firstname, self.lastname, self.password)
+        agentdomain = AgentDomain(self.login_uri)
+
+        #gets seedcap, and an agent that can be placed in a region
+        agent = agentdomain.login(credentials)
+
+        #establish AD event queue connection
+        ad_event_queue = IEventQueueGet(agentdomain)
+        ad_queue_thread = Thread(target=run_ad_eq, name="Agent Domain event queue", args=(ad_event_queue,))
+        ad_queue_thread.start()
+        
+        #gets the rez_avatar/place cap
+        caps = agentdomain.seed_cap.get(['rez_avatar/place'])
+        
+        # try and rez on a sim
+        region = Region(self.start_region_uri)
+        place = IPlaceAvatar(agentdomain)
+        avatar = place(region, [5,5,5])
+        
+        # grab some data before it disappears
+        vanishing_data = {}
+        vanishing_data['agent_id'] = avatar.region.details['agent_id']
+        vanishing_data['session_id'] = avatar.region.details['session_id']
+        vanishing_data['circuit_code'] = avatar.region.details['circuit_code']        
+                
+        # grab some packets from sim1
+        packets_captured = self.maintain_sim_presence(vanishing_data, region, place, avatar, 5)
+
+        # try and rez on sim2
+        region2 = Region(self.target_region_uri)
+        place2 = IPlaceAvatar(agentdomain)
+        avatar2 = place(region2, [5,5,5])
+
+        # grab some packets from sim2
+        packets_captured2 = self.maintain_sim_presence(vanishing_data, region2, place2, avatar2, 5)
+        
+        # now let's validate some shizz
+        # assert avatar.region.details['seed_capability'] != avatar2.region.details['seed_capability']
+        assert packets_captured2['TeleportProgress'] == 1
+        assert packets_captured2['AgentMovementComplete'] == 1
+
+    def maintain_sim_presence(self, vanishing_data, region, place, avatar, duration):
+
+        self.agent_id = vanishing_data['agent_id']
+        self.session_id = vanishing_data['session_id']
+        self.circuit_code = vanishing_data['circuit_code']       
+    
         #begin UDP communication
         self.host = IHost((avatar.region.details['sim_ip'],
                     avatar.region.details['sim_port']))
 
         self.connect(self.host, avatar.region)
         
-        print "Entering loop"
+        #print "Entering loop"
         last_ping = 0
-        ad_event_queue = IEventQueueGet(agentdomain)
+        
         sim_event_queue = IEventQueueGet(region)
 
-        from threading import Thread
-        ad_queue_thread = Thread(target=run_ad_eq, name="Agent Domain event queue", args=(ad_event_queue,))
         sim_queue_thread = Thread(target=run_sim_eqg, name="Agent Domain event queue", args=(sim_event_queue,))
-        if canInput == True:
-            input_thread = Thread(target=run_input_check, name="Thread to handle input", args=(self, agentdomain))
-            input_thread.start()
-        ad_queue_thread.start()
-        sim_queue_thread.start()
         
-        last_time = time.time()
-        update_count = 0
+        sim_queue_thread.start()
 
-        while True:
+        last_ping = 0
+        start = time.time()
+        now = start
+        packets = {}
+        
+        while ((now - start) < 15):
             msg_buf, msg_size = self.messenger.udp_client.receive_packet(self.messenger.socket)
             packet = self.messenger.receive_check(self.messenger.udp_client.get_sender(),
                                             msg_buf, msg_size)
             if packet != None:
-                print 'Received: ' + packet.name + ' from  ' + self.messenger.udp_client.sender.ip + ":" + \
-                                                  str(self.messenger.udp_client.sender.port)
+                #print 'Received: ' + packet.name + ' from  ' + self.messenger.udp_client.sender.ip + ":" + \
+                 #                                 str(self.messenger.udp_client.sender.port)
 
                 #MESSAGE HANDLERS
                 if packet.name == 'RegionHandshake':
@@ -171,20 +244,24 @@ class OGPTeleportTest(unittest.TestCase):
                 elif packet.name == 'StartPingCheck':
                     self.send_complete_ping_check(last_ping)
                     last_ping += 1                    
-
+                    
+                if packet.name not in packets:
+                    packets[packet.name] = 1
+                else: 
+                    packets[packet.name] += 1  
+                                    
+            else:
+                #print 'No message'
+                pass
+                
             if self.messenger.has_unacked():
+                #print 'Acking'
                 self.messenger.process_acks()
-    
-            current_time = time.time()
-            if current_time - last_time >= 1:
-                update_count = 0
-    
-            #update 10 times a second max
-            if update_count < 10: 
-                update_count += 1
-                self.send_agent_update(self.agent_id, self.session_id, globals()["controlFlags"])
-                print "Control flags: " + repr(globals()["controlFlags"])
-                globals()["agentFlagSent"] = globals()["controlFlags"]
+                self.send_agent_update(self.agent_id, self.session_id)
+        
+            now = time.time()
+        
+        return packets    
 
     def connect(self, host, region):
         #SENDS UseCircuitCode
@@ -218,19 +295,20 @@ class OGPTeleportTest(unittest.TestCase):
                       )
         self.messenger.send_message(msg, self.host)
 
-    def send_agent_update(self, agent_id, session_id, control_flag):
+
+    def send_agent_update(self, agent_id, session_id):
         msg = Message('AgentUpdate',
                       Block('AgentData', AgentID=uuid.UUID(agent_id),
                             SessionID=uuid.UUID(self.session_id),
-                            BodyRotation=(0.0,0.0,0.0),
-                            HeadRotation=(0.0,0.0,0.0),
+                            BodyRotation=(0.0,0.0,0.0,0.0),
+                            HeadRotation=(0.0,0.0,0.0,0.0),
                             State=0x00,
                             CameraCenter=(0.0,0.0,0.0),
-                            CameraAtAxis=(1.0,0.0,0.0),
-                            CameraLeftAxis=(0.0,1.0,0.0),
-                            CameraUpAxis=(0.0,0.0,1.0),
-                            Far=1.0,
-                            ControlFlags=control_flag,
+                            CameraAtAxis=(0.0,0.0,0.0),
+                            CameraLeftAxis=(0.0,0.0,0.0),
+                            CameraUpAxis=(0.0,0.0,0.0),
+                            Far=0,
+                            ControlFlags=0x00,
                             Flags=0x00))
 
         self.messenger.send_message(msg, self.host)
@@ -252,76 +330,29 @@ class OGPTeleportTest(unittest.TestCase):
 #for threading
 def run_ad_eq(ad_event_queue):
     #NOW, event queue to sim
-    while True:
+    
+    start = time.time()
+    now = start
+    
+    while (now - start > 15):
         result = ad_event_queue()
-        print "Agent Domain queue returned: "
-        pprint.pprint(result)
+        #print "Agent Domain queue returned: "
+        #pprint.pprint(result)
 
 def run_sim_eqg(sim_event_queue):
-    while True:
+    
+    start = time.time()
+    now = start
+    
+    while (now - start > 15):
         try:
             result = sim_event_queue()
-            print "Sim event queue returned: "
-            pprint.pprint(result)
+            #print "Sim event queue returned: "
+            #pprint.pprint(result)
         except Exception, e:
-            print "Sim had no events"
+            #print "Sim had no events"
             #just means nothing to get
             pass
-
-def run_input_check(teleporter, agentdomain):
-    while True:
-        #wait til press
-        while not msvcrt.kbhit():
-            pass
-
-        c = msvcrt.getch()
-        if c == '\xe0':
-            kk = msvcrt.getch()
-            if kk == 'H':
-                if globals()["agentFlagSent"] != NUDGE_FORWARD:
-                    print 'NUDGING FORWARD'
-                    globals()["controlFlags"] = NUDGE_FORWARD
-                else:
-                    print 'MOVE UP'
-                    globals()["controlFlags"] = MOVE_FORWARD
-            elif kk == 'P':
-                print 'MOVE DOWN'
-                if globals()["agentFlagSent"] != NUDGE_BACKWARD:
-                    globals()["controlFlags"] = NUDGE_BACKWARD
-                else:
-                    globals()["controlFlags"] = MOVE_BACKWARD
-            elif kk == 'K':
-                print 'MOVE LEFT'
-                if globals()["agentFlagSent"] != NUDGE_LEFT:
-                    globals()["controlFlags"] = NUDGE_LEFT
-                else:
-                    globals()["controlFlags"] = MOVE_LEFT
-            elif kk == 'M':
-                print 'MOVE RIGHT'
-                if globals()["agentFlagSent"] != NUDGE_RIGHT:
-                    globals()["controlFlags"] = NUDGE_RIGHT
-                else:
-                    globals()["controlFlags"] = MOVE_RIGHT
-                
-            print kk
-        else:
-            #SPACE HALTS THE AGENT
-            if c == ' ':
-                globals()["controlFlags"] = STOP_MOVING
-            elif c == 't':
-                print 'TELEPORTING'
-                #READ THE TARGET REGION URL from a file
-                tp_region = Region(teleporter.target_region_uri)
-                place = IPlaceAvatar(agentdomain)
-
-                avatar = place(tp_region)        
-
-                host = IHost((avatar.region.details['sim_ip'],
-                            avatar.region.details['sim_port']))                
-
-                teleporter.connect(host, avatar.region)
-
-            print repr(c)    
             
 
 def test_suite():
